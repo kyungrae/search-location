1667c0c285198e9984f454b8ae977668838e0bed
kyungrae <rlarudfo93@gmail.com>
2023-03-18 13:23:26 +0900

    프로젝트 초기 설정 & readme 파일 작성

---
+++ src/main/kotlin/hama/searchlocation/SearchLocationApplication.kt
@@ -0,0 +1,11 @@
+package hama.searchlocation
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SearchLocationApplication
+
+fun main(args: Array<String>) {
+    runApplication<SearchLocationApplication>(*args)
+}
---
+++ src/test/kotlin/hama/searchlocation/SearchLocationApplicationTests.kt
@@ -0,0 +1,13 @@
+package hama.searchlocation
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SearchLocationApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
